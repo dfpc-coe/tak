80b3f7c577a663fc0b26efb6ce12ce3e1303d7a0
ingalls <nick@ingalls.ca>
2022-10-05 21:54:41 -0600

    Add Root User Secret

--- cloudformation/lib/secret.js
+++ cloudformation/lib/secret.js
@@ -2,13 +2,19 @@ import cf from '@mapbox/cloudfriend';
 
 export default {
     Resources: {
-        APISecrets: {
+        RootSecret: {
             Type: 'AWS::SecretsManager::Secret',
             Properties: {
-                Description: cf.join([cf.stackName, ' API Secrets']),
-                Name: cf.join([cf.stackName, '/api']),
+                Description: cf.join([cf.stackName, ' Root User']),
+                GenerateSecretString: {
+                    SecretStringTemplate: '{"username": "root"}',
+                    GenerateStringKey: 'password',
+                    ExcludePunctuation: true,
+                    PasswordLength: 32
+                },
+                Name: cf.join([cf.stackName, '/user/root']),
                 KmsKeyId: cf.ref('KMS')
             }
-        }
+        },
     }
 };
